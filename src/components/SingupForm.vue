e38637ffcc627e64700690caf94820bd4d3790b8
halac <halac122@gmail.com>
2021-09-16 16:47:54 +0300

    Added Some Features

---
+++ src/components/SingupForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <input type="text" required placeholder="display name" v-model="displayName">
+        <input type="email" required placeholder="email" v-model="email">
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyBGo60TBeYploqzlHFgIclwCFPqVtoEH48",
+    authDomain: "abdirisak-firebase-site.firebaseapp.com",
+    projectId: "abdirisak-firebase-site",
+    storageBucket: "abdirisak-firebase-site.appspot.com",
+    messagingSenderId: "428907351709",
+    appId: "1:428907351709:web:b68084e570df026bd78e74"
+  };
+
+  // init firebase 
+  firebase.initializeApp(firebaseConfig)
+
+  const firebaseConfig  = firebase.firestore()
+
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+  
