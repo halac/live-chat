fa33502da4eedcc033ead9d87cd1f336a20ac424
halac <halac122@gmail.com>
2021-09-20 10:06:29 +0300

    Updated Some Features

--- src/composables/getCollection.js
+++
@@ -1,9 +0,0 @@
-import {ref} from 'vue'
-import { projectFirestore } from '../firebase/config'
-
-const getCollection = (collection) => {
-    const documents = ref(null)
-
-}
-
-export default getCollection
