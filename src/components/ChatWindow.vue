fb5cf132d6265b58694446fc7e93a39cab9bc8fa
halac <halac122@gmail.com>
2021-09-20 11:44:43 +0300

    Added Some Features

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chat-window">
+      <div v-if="error"> {{error}} </div>
+      <div v-for="doc in documents" :key="doc.id" class="single">
+          <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+          <span class="name">{{ doc.name }}</span>
+          <span class="message"> {{ doc.message }}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+import getCollection from '../composables/getCollection'
+export default {
+    setup() {
+        const {error, documents} = getCollection('messages')
+
+        return {error, documents}
+    }
+}
+</script>
+
+<style>
+
+</style>
